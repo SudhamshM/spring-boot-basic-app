ade9665235bf72a5764daefb2e386f00e274b32c
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-09-10 14:38:18 -0400

    Move student routes to separate controller

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -16,15 +15,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@GetMapping
-	public List<Student> hello() {
-		return List.of(new Student(17L,
-				"John Wick",
-				"john@wick.com",
-				LocalDate.of(2005, Month.APRIL, 15),
-				28));
 	}
 
 }
